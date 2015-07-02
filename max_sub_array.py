d3d5d3353e42055d60c08a8d995fa3a6ddb1bb54
fitzsimk <kfitzsimmons11@gmail.com>
2015-07-02 14:52:49 -0700

    add object container for functions

--- max_sub_array.py
+++ max_sub_array.py
@@ -4,16 +4,22 @@
 # Course:         CS325-400                                          #
 # Assignment:     Project 1                                          #
 # File:           max_sub_array.py                                   #
-# Description:    Four algorithms for solving max sub array problem  #
+# Description:    Object that contains Four algorithms that solve    #
+#                 the max sub array problem                          #
 ######################################################################
+class MaxSub(object):
+    # Algorithm 1: Enumeration
+    #   enumeration for max subarray
+    #   evaluates every possible solution
+    #   analysis (O(n^2) pairs) x (O(n) time to compute each sum)= O(n^3) time
 
-# enumeration for max subarray
-# evaluates every possible solution
+    # def MaxSubEnum(a):
+        # for each pair(i,j) with 1 <= i <= j <= n
+        #   compute a[i]+a[i+1]+...+a[j-1]+a[j]
+        #   keep max sum found so far
+        # keep max sum found
 
-def MaxSubEnum(a):
-    # for each pair(i,j) with 1 <= i <= j <= n
-    #   compute a[i]+a[i+1]+...+a[j-1]+a[j]
-    #   keep max sum found so far
-    # keep max sum found
 
-# analysis (O(n^2) pairs) x (O(n) time to compute each sum)= O(n^3) time
+
+
+    #Algorithm 2: Better Enumeration
