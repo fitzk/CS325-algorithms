7047f4d5331bd8e9936d94928565b48a8e7747fa
fitzsimk <kfitzsimmons11@gmail.com>
2015-07-02 14:53:46 -0700

    add timer.py times algorithms in MaxSub object

---
+++ timer.py
@@ -0,0 +1,37 @@
+######################################################################
+# Project Group:  1                                                  #
+# Group Members:  Eric Cruz, Baljot Sighn, Kayla Fitzsimmons         #
+# Course:         CS325-400                                          #
+# Assignment:     Project 1                                          #
+# File:           timer.py                                           #
+# Description:    Calculates runtime for max sub array algorithms    #
+######################################################################
+from max_sub_array import MaxSub
+
+#set up command line args for inputing testData
+
+#instance of MaxSub object
+maxSub = maxSub()
+
+#creates a file for output
+f = open('output','w')
+
+#header for output
+header = Size of Input + '\t'+ Maximum Subarray +'\t'+ Run Time +'\n'
+h_value = str(header)
+f.write(h_value)
+
+# time algorithm
+start = time.time()
+result = maxSub.# function to be tested
+end = time.time()
+
+# format output
+r = #repr(size of input) + '\t'+ repr(result) +'\t'+repr(end - start)+'\n'
+r_value = str(r)
+
+# write result to file
+f.write(r_value)
+
+# close file
+f.close()
