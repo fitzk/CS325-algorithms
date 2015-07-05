2dfecc463f39319c0e472718c3b200a872837552
fitzsimk <kfitzsimmons11@gmail.com>
2015-07-05 13:53:10 -0700

    add timer and output for algorithm 3

--- timer.py
+++ timer.py
@@ -31,35 +31,48 @@ f2 = open('MSS_Results.txt','w')
 
 
 for line in f:
-  array =  map(int, re.findall(r"[-+]?\d*\-\d+|\d+", line))
+    array =  map(int, re.findall(r"[-+]?\d*\-\d+|\d+", line))
 
-	# time algorithm1
-	start = time.clock()
-	result, arr = maxSub.maxSubEnum(array) # function to be tested
-	end = time.clock()
-	
-	#Write to file results of algorithm1
-	s = 'Algorithm 1' + '\n' + str(array)+'\n' + str(arr) + '\n' + str(result) + '\n' + 'Time Taken: ' + str(end - start) + '\n' + '\n'
-	f2.write(s)
-	
-	#empty arr
-	del arr[:]
-	arr[:] = []
-	
-	# time algorithm2
-	start = time.clock()
-	result, arr = maxSub.maxSubEnum2(array) # function to be tested
-	end = time.clock()
-	
-	#Write to file results of algorithm2
-	s = 'Algorithm 2' + '\n' + str(array)+'\n' + str(arr) + '\n' + str(result) + '\n' + 'Time Taken: ' + str(end - start) + '\n' + '\n'
-	f2.write(s)
+    # time algorithm1
+    start = time.clock()
+    result, arr = maxSub.maxSubEnum(array) # function to be tested
+    end = time.clock()
 
-	#empty arr and array
-	del arr[:]
-	arr[:] = []
-	del array[:]
-	array[:] = []
+    #Write to file results of algorithm1
+    s = 'Algorithm 1' + '\n' + str(array)+'\n' + str(arr) + '\n' + str(result) + '\n' + 'Time Taken: ' + str(end - start) + '\n' + '\n'
+    f2.write(s)
+
+    #empty arr
+    del arr[:]
+    arr[:] = []
+
+    # time algorithm2
+    start = time.clock()
+    result, arr = maxSub.maxSubEnum2(array) # function to be tested
+    end = time.clock()
+
+    #Write to file results of algorithm2
+    s = 'Algorithm 2' + '\n' + str(array)+'\n' + str(arr) + '\n' + str(result) + '\n' + 'Time Taken: ' + str(end - start) + '\n' + '\n'
+    f2.write(s)
+
+    #empty arr and array
+    del arr[:]
+    arr[:] = []
+
+    # time algorithm3
+    start = time.clock()
+    low, high, result = maxSub.find_max_sub_array(array,0,len(array)-1) # function to be tested
+    end = time.clock()
+
+    #Write to file results of algorithm3
+    s = 'Algorithm 3' + '\n' + str(array)+'\n' + str(arr) + '\n' + str(result) + '\n' + 'Time Taken: ' + str(end - start) + '\n' + '\n'
+    f2.write(s)
+
+    #empty arr and array
+    del arr[:]
+    arr[:] = []
+    del array[:]
+    array[:] = []
 
 # close file
 f.close()
